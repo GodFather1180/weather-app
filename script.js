87d6df81c9101ef76241204850c9fda99f0c915c
GodFather1180 <92819436+GodFather1180@users.noreply.github.com>
2024-09-30 00:24:59 -0700

    wow

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'e80c38e7e9mshc99cb8b70b1a2cep146fdbjsn159175eae0c7',
+		'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
